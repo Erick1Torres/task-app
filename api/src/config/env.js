544e16e2d15e3cc54bd0def30b2e5c8224a22a41
Erick1Torres <erickcacerestorres@gmail.com>
2026-04-07 12:32:28 +0200

    update

--- api/src/config/env.js
+++ api/src/config/env.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
 module.exports = {
-    // Si no hay puerto en el .env, usa el 3000 por defecto
-    // En Vercel, esto simplemente se ignorará o se adaptará solo
     PORT: process.env.PORT || 3000 
 };
